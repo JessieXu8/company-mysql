12c210ff7db9eb2cf844683ec2416681fb6c64bf
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-29 13:01:59 +0800

    [jessie] complete the test of add employee

--- src/test/java/com/oocl/companymysql/controllers/EmployeeControllerTest.java
+++ src/test/java/com/oocl/companymysql/controllers/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package com.oocl.companymysql.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oocl.companymysql.entities.Employee;
 import com.oocl.companymysql.services.EmployeeService;
 import org.junit.Test;
@@ -7,14 +8,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,6 +29,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class EmployeeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @MockBean
     private EmployeeService employeeService;
@@ -39,5 +48,15 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$[0].age").value(20))
                 .andExpect(jsonPath("$[0].gender").value("male"))
                 .andExpect(jsonPath("$[0].salary").value(6000));
+    }
+
+    @Test
+    public void should_return_employee_when_call_addEmployee()throws Exception{
+        Employee employee = new Employee("bob",20,"male",5000);
+        when(employeeService.addEmployee(any(Employee.class))).thenReturn(employee);
+        mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(employee)))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
     }
 }
